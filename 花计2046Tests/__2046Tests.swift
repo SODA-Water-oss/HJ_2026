f2a3cd470da8b12c2e29ff03d8a7d5b13fa17ae8
SODA <vwhat@qq.com>
2026-07-26 09:44:09 +0800

    test: add MathCalculator unit tests

--- 花计2046Tests/__2046Tests.swift
+++ 花计2046Tests/__2046Tests.swift
@@ -17,3 +17,43 @@ struct __2046Tests {
     }
 
 }
+
+struct MathCalculatorTests {
+
+    @Test func simpleAddition() {
+        #expect(MathCalculator.evaluate("35+20") == 55)
+    }
+
+    @Test func multiplicationPrecedence() {
+        #expect(MathCalculator.evaluate("3+2*4") == 11)
+    }
+
+    @Test func pureText_noFormula() {
+        #expect(MathCalculator.containsFormula("午餐") == false)
+    }
+
+    @Test func chineseMixedText() {
+        #expect(MathCalculator.preprocess("午餐35+20超市") == "午餐55 (35+20)超市")
+    }
+
+    @Test func chainedAddition() {
+        #expect(MathCalculator.preprocess("35+20+15") == "70 (35+20+15)")
+    }
+
+    @Test func zeroInput_returnsNil() {
+        #expect(MathCalculator.evaluate("") == nil)
+    }
+
+    @Test func fullWidthOperators() {
+        #expect(MathCalculator.evaluate("3×4") == 12)
+        #expect(MathCalculator.evaluate("8÷2") == 4)
+    }
+
+    @Test func subtraction() {
+        #expect(MathCalculator.evaluate("100-35") == 65)
+    }
+
+    @Test func division() {
+        #expect(MathCalculator.evaluate("10/3") == (10.0 / 3.0))
+    }
+}
